a5cf915237383273c1c8851dcc924ba1b631a3f7
galtreger <125656208+tregergal@users.noreply.github.com>
2024-09-17 14:59:24 +0200

    1

---
+++ mergeScript.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# %O = ancestor version (common base of merge)
+# %A = current branch version (ours)
+# %B = other branch version (theirs)
+
+# The script is expected to return a 0 exit status if it successfully
+# merges the files and a non-zero exit status otherwise.
+
+# Check if the stacktic-stag directory exists in ours
+if [ -d "$2/README.md" ]; then
+    # Do nothing, just copy the current (ours) version
+    cp -r $2/README.md $2/
+else
+    # If it doesn't exist in ours, fallback to the common ancestor version
+    cp -r $1/README.md $2/ || true
+fi
+
+# Always succeed the custom merge
+exit 0
