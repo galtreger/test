5384a7174d910b93051f6359f9dfe9f3eb9683c9
galtreger <125656208+tregergal@users.noreply.github.com>
2024-09-17 15:06:17 +0200

    1

--- mergeScript.sh
+++ mergeScript.sh
@@ -8,12 +8,12 @@
 # merges the files and a non-zero exit status otherwise.
 
 # Check if the stacktic-stag directory exists in ours
-if [ -d "$2/README.md" ]; then
+if [ -d "$2/test/" ]; then
     # Do nothing, just copy the current (ours) version
-    cp -r $2/README.md $2/
+    cp -r $2/test/* $2/
 else
     # If it doesn't exist in ours, fallback to the common ancestor version
-    cp -r $1/README.md $2/ || true
+    cp -r $1/test/* $2/ || true
 fi
 
 # Always succeed the custom merge
